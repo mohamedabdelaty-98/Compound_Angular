1d2b30b933bd622980d6a95ca5fbdf608afe46a4
Shrouk Hassan <68817581+ShrouqHassan@users.noreply.github.com>
2023-10-28 15:14:59 +0200

    Compound Slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompoundSliderComponent } from './Components/compound-slider/compound-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompoundSliderComponent
   ],
   imports: [
     BrowserModule,
