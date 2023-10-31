67397107e78dc90297ed83de28be9460b713a698
mohamedabdelaty-98 <abdelatym775@gmail>
2023-11-01 01:18:49 +0200

    fffff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SliderHomeComponent } from './component/slider-home/slider-home.component';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CarouselModule } from 'ngx-owl-carousel-o';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, SliderHomeComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    CarouselModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
