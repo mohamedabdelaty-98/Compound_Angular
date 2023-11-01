8b2d4aea4bbf995d7dba3cf11355e2bd3d701168
ahmedGalbatx <galbatahmed122@gmail.com>
2023-11-01 00:49:41 -0700

    video-static

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandmarksComponent } from './Components/landmarks/landmarks.component';
+import { StaticVideoComponent } from './Components/static-video/static-video.component';
+import { AboutsectionComponent } from './Components/aboutsection/aboutsection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandmarksComponent
+    LandmarksComponent,
+    StaticVideoComponent,
+    AboutsectionComponent
   ],
   imports: [
     BrowserModule,
