36c593383683d051deb8c592c312120bbf427cce
Amr Ewies <amrewies1998@gmail.com>
2023-11-02 09:33:49 +0200

    Filter Cart Footer

--- src/app/Component/card/card.component.ts
+++ src/app/Component/card/card.component.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
   currentPage = 1;
+ numCards = 26; // Replace with the actual number of cards
 
+ cardsPerPage = 9; // Number of cards to display per page
+   numPages = Math.ceil(this.numCards / this.cardsPerPage); // Calculate the number of pages
+  
   showPage(pageNumber: number) {
     this.currentPage=pageNumber;
   const cards = document.querySelectorAll('.card');
