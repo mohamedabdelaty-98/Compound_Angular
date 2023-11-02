75135c5fd66998f4f4ad71ad926dc3e5a14232c4
mohamedabdelaty-98 <abdelatym775@gmail>
2023-11-02 12:48:22 +0200

    part of salah

--- src/app/Components/Home/aboutsection/aboutsection.component.ts
+++ src/app/Components/Home/aboutsection/aboutsection.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aboutsection',
   templateUrl: './aboutsection.component.html',
-  styleUrls: ['./aboutsection.component.css']
+  styleUrls: ['./aboutsection.component.css'],
 })
-export class AboutsectionComponent {
-
-}
+export class AboutsectionComponent {}
