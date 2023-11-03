a8612622b6f218f994fee917ffac58c4dfe36d2d
mohamedabdelaty-98 <abdelatym775@gmail>
2023-11-03 03:29:55 +0200

    dd

---
+++ src/app/Components/ammenities-compound/ammenities-compound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ammenities-compound',
+  templateUrl: './ammenities-compound.component.html',
+  styleUrls: ['./ammenities-compound.component.css']
+})
+export class AmmenitiesCompoundComponent {
+
+}
