33bf048dd7266698d21840b9c39f4d635cd214b4
mohamedabdelaty-98 <abdelatym775@gmail>
2023-11-06 17:42:19 +0200

    update all work

--- src/app/Components/Units/filter/filter.component.ts
+++ src/app/Components/Units/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GitFloorsService } from 'src/app/Services/Units/git-floors.service';
 
@@ -7,7 +7,7 @@ import { GitFloorsService } from 'src/app/Services/Units/git-floors.service';
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css'],
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent {
   compoundid: number = 3;
   floorsdata: string[] = [];
   constructor(
